9dd6326df6eb484bea5d8dc58dbcef2c83a9e1c9
dario <dariogomezperez@gmail.com>
2022-01-24 14:59:11 +0100

    grammar on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,22 +23,22 @@ document.addEventListener('DOMContentLoaded', function() {
         }            
         if (textInputValueLowerCase.substr(0,5) == "open ") { //if the first 5 characters = open + space
           openLinkInNewWindow('http://' + textInputValueLowerCase.substr(5) );
-          addTextToResults("<i>The URL " + "<b>" + textInputValue.substr(5) + "</b>" + " should be opened now.</i>");
+          addTextToResults("<i>The URL " + "<b>" + textInputValue.substr(5) + "</b>" + " should be open now.</i>");
           return;
         } 
         if (textInputValueLowerCase.substr(0,8) == "youtube ") {
           openLinkInNewWindow('https://www.youtube.com/results?search_query=' + textInputValueLowerCase.substr(8));
-          addTextToResults("<i>I've searched on YouTube for " + "<b>" + textInputValue.substr(8) + "</b>" + " it should be opened now.</i>");
+          addTextToResults("<i>I've searched on YouTube for " + "<b>" + textInputValue.substr(8) + "</b>" + " it should be open now.</i>");
           return;
         } 
         if (textInputValueLowerCase.substr(0,7) == "google ") {
           openLinkInNewWindow('https://www.google.com/search?q=' + textInputValueLowerCase.substr(7));
-          addTextToResults("<i>I've searched on Google for " + "<b>" + textInputValue.substr(7) + "</b>" + " it should be opened now.</i>");
+          addTextToResults("<i>I've searched on Google for " + "<b>" + textInputValue.substr(7) + "</b>" + " it should be open now.</i>");
           return;
         } 
         if (textInputValueLowerCase.substr(0,5) == "wiki "){
           openLinkInNewWindow('https://wikipedia.org/w/index.php?search=' + textInputValueLowerCase.substr(5));
-          addTextToResults("<i>I've searched on Wikipedia for " + "<b>" + textInputValue.substr(5) + "</b>" + " it should be opened now.</i>");
+          addTextToResults("<i>I've searched on Wikipedia for " + "<b>" + textInputValue.substr(5) + "</b>" + " it should be open now.</i>");
           return;
         } else {
             postHelpList();
